111fb97ffb09bb712a4750978c71bca64ce38f7c
austinaeden <austin.mbogo@student.moringaschool.com>
2023-06-22 07:56:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// code your solution here 
+
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun('bathe my dog')
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork('work from home')
+
+function wrapAdjective(result=('*')){
+ function adjective(emphatic='a dedicated programmer'){
+    return`You are ${result}${emphatic}${result}!`
+}
+return adjective
+}
+
